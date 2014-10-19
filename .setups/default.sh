4ba936c97743f6b47a9b22be9ba6884d0cde94fc
Francisco Arámburo <atfm05@gmail.com>
2014-10-18 20:20:35 -0400

    Little bug

--- .setups/default.sh
+++ .setups/default.sh
@@ -1,10 +1,10 @@
-mv ../.bashrc ../.bash_prompt ../.gitconfig ../.git ../.gitignore ../.vimrc . ~
+mv ../.bashrc ../.bash_prompt ../.gitconfig ../.git ../.gitignore ../.vimrc ../.setups ~
 touch ~/.exports
 touch ~/.aliases
 touch ~/.functions
 mkdir ~/Library
 mkdir ~/Repositories
-rm -rf ..
+rm -rf ~/d
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall +qall
 
